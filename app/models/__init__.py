f3d05e128ee694315e2fb4c7c8caec5e980ed337
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-22 19:51:26 +0900

    part01/chapter-01: 프로젝트 구조 생성

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.category import Category, CategoryCreate, CategoryUpdate
+from app.models.product import Product, ProductCreate, ProductUpdate, ProductImage
+
+__all__ = [
+    "Category", "CategoryCreate", "CategoryUpdate",
+    "Product", "ProductCreate", "ProductUpdate", "ProductImage",
+]
